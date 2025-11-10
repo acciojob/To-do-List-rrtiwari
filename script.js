d0b396cf7624b283ff0f684e4d687770eb03f0a7
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-10 09:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
-
+function validate(data){
+	if(data==="")
+		  return false;
+     else
+		  return true;
+}
+document.getElementById("addTodoBtn").addEventListener("click",(e)=>{
+	e.preventDefault();
+    let data=document.getElementById("newTodoInput").value;
+	if(validate(data)){
+	let li=document.createElement("li");
+	li.innerHTML=data;
+	let ol=document.getElementById("todoList");
+	ol.appendChild(li);
+	document.getElementById("newTodoInput").value="";
+   }
+})
